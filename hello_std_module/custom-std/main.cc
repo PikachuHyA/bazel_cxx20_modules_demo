3387198abc91af593158ddaeee2558add0d80948
PikachuHy <pikachuhya@gmail.com>
2026-09-14 20:26:28 +0800

    update std module demo, use new impl

---
+++ hello_std_module/custom-std/main.cc
@@ -0,0 +1,5 @@
+import std;
+
+int main() {
+    std::println("custom std_module target works");
+}
---
+++ hello_std_module/fallback/main.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "empty std target resolves" << std::endl;
+}
